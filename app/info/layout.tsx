748fe9ba4a498380938b2f12d54a30e51c91ad8f
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-10-29 16:05:21 +0800

    29.10.24.1604

---
+++ app/info/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+import InfoPage from "./page";
+
+export const metadata: Metadata = {
+  title: "About Me",
+  description: "Info Page",
+};
+
+export default InfoPage;
