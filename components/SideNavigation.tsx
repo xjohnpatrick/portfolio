a69eefdf3cf01bd9837ce2cd4048046ead2a2dbc
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-02 13:11:23 +0800

    02.11.24.1311

--- components/SideNavigation.tsx
+++ components/SideNavigation.tsx
@@ -4,25 +4,44 @@ import { SlArrowUp } from "react-icons/sl";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { TfiClose } from "react-icons/tfi";
 import { SlArrowDown } from "react-icons/sl";
+import { TransitionLink } from '@/utils/TransitionLink';
 
 export default function SideNavigation() {
-    const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-    const toggleHover = (hovering: boolean) => setIsHovered(hovering);
+  const toggleHover = (hovering: boolean) => setIsHovered(hovering);
+
+  // Scroll up by 500px with smooth animation
+  const scrollUp = () => {
+    window.scrollBy({ top: -1000, behavior: "smooth" });
+  };
+
+  // Scroll down by 500px with smooth animation
+  const scrollDown = () => {
+    window.scrollBy({ top: 1000, behavior: "smooth" });
+  };
 
   return (
     <div className="flex flex-col justify-center fixed right-0 top-1/2 -translate-y-1/2 z-50 gap-[1px]">
-      <button className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black">
+      <button
+        onClick={scrollUp}
+        className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black"
+      >
         <SlArrowUp size={16} />
       </button>
+      <TransitionLink href="/all-projects">
+        <button
+          className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black"
+          onMouseEnter={() => toggleHover(true)}
+          onMouseLeave={() => toggleHover(false)}
+        >
+          {isHovered ? <TfiClose size={16} /> : <RxHamburgerMenu size={16} />}
+        </button>
+      </TransitionLink>
       <button
+        onClick={scrollDown}
         className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black"
-        onMouseEnter={() => toggleHover(true)}
-        onMouseLeave={() => toggleHover(false)}
       >
-        {isHovered ? <TfiClose size={16} /> : <RxHamburgerMenu size={16} />}
-      </button>
-      <button className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black">
         <SlArrowDown size={16} />
       </button>
     </div>
