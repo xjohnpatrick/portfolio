33ecdb9a5344ad83a39f2d16b0d7152433999ef2
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-05 04:12:45 +0800

    05.11.24.0412

--- components/SideNavigation.tsx
+++ components/SideNavigation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { SlArrowUp } from "react-icons/sl";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { TfiClose } from "react-icons/tfi";
@@ -7,10 +7,6 @@ import { SlArrowDown } from "react-icons/sl";
 import { TransitionLink } from "@/utils/TransitionLink";
 
 export default function SideNavigation() {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const toggleHover = (hovering: boolean) => setIsHovered(hovering);
-
   const scrollUp = () => {
     window.scrollBy({ top: -1000, behavior: "smooth" });
   };
@@ -21,27 +17,50 @@ export default function SideNavigation() {
 
   return (
     <div className="flex flex-col justify-center fixed right-0 top-1/2 -translate-y-1/2 z-50 gap-[1px]">
-      <button
-        onClick={scrollUp}
-        className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black"
-      >
-        <SlArrowUp size={16} />
-      </button>
+      <div className="boxNav w-10 h-10">
+        <div className="boxNav-inner relative w-full h-full">
+          <button
+            onClick={scrollUp}
+            className="flex w-full h-full absolute bg-black text-white items-center justify-center boxNav-front"
+          >
+            <SlArrowUp size={16} />
+          </button>
+          <button
+            onClick={scrollUp}
+            className="flex w-full h-full absolute bg-white text-black items-center justify-center boxNav-back"
+          >
+            <SlArrowUp size={16} />
+          </button>
+        </div>
+      </div>
       <TransitionLink href="/all-projects">
-        <button
-          className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black"
-          onMouseEnter={() => toggleHover(true)}
-          onMouseLeave={() => toggleHover(false)}
-        >
-          {isHovered ? <RxHamburgerMenu size={16} /> : <TfiClose size={16} />}
-        </button>
+        <div className="boxNav3 w-10 h-10">
+          <div className="boxNav3-inner w-full h-full relative">
+            <button className="flex bg-black w-full h-full absolute text-white items-center justify-center boxNav3-front">
+              <TfiClose size={16} />
+            </button>
+            <button className="flex bg-white w-full h-full absolute text-black items-center justify-center boxNav3-back">
+              <RxHamburgerMenu size={16} />
+            </button>
+          </div>
+        </div>
       </TransitionLink>
-      <button
-        onClick={scrollDown}
-        className="flex w-10 h-10 bg-black text-white items-center justify-center hover:bg-white hover:text-black"
-      >
-        <SlArrowDown size={16} />
-      </button>
+      <div className="boxNav2 w-10 h-10">
+        <div className="boxNav2-inner relative w-full h-full">
+          <button
+            onClick={scrollDown}
+            className="flex w-full h-full absolute bg-black text-white items-center justify-center boxNav2-front"
+          >
+            <SlArrowDown size={16} />
+          </button>
+          <button
+            onClick={scrollDown}
+            className="flex w-full h-full absolute bg-white text-black items-center justify-center boxNav2-back"
+          >
+            <SlArrowDown size={16} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
