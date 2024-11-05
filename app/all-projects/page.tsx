d83f68bc564fe0b7dce5d079ae72bb4dc428d375
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-05 17:59:33 +0800

    05.11.24.1759

--- app/all-projects/page.tsx
+++ app/all-projects/page.tsx
@@ -8,9 +8,7 @@ export default function AllProjects() {
     <div className="flex flex-col w-full h-screen items-center p-6">
       <div className="w-full md:w-[750px]">
         <div className="flex flex-col w-full">
-          <span
-            className="uppercase leading-3 text-gray text-base mt-4 font-bebas"
-          >
+          <span className="uppercase leading-3 text-gray text-base mt-4 font-bebas">
             2023-2024
           </span>
           <span className="uppercase text-white text-3xl mb-4 font-bebas">
@@ -19,35 +17,54 @@ export default function AllProjects() {
         </div>
         <div className="flex flex-col w-full gap-10">
           {projects.map((proj) => (
-            <div key={proj.id} className="w-full h-[240px] box">
-              <TransitionLink href={`/projects/${proj.id}`} passHref>
-                <div className="box-inner relative w-full h-full">
-                  <Image
-                    src={proj.img[0]}
-                    alt={proj.alt}
-                    fill
-                    className="object-cover box-front"
-                  />
+            <>
+              {/* FOR LARGE DEVICES */}
+              <div
+                key={proj.id}
+                className="hidden sm:block w-full h-[240px] box"
+              >
+                <TransitionLink href={`/projects/${proj.id}`} passHref>
+                  <div className="box-inner relative w-full h-full">
+                    <Image
+                      src={proj.img[0]}
+                      alt={proj.alt}
+                      fill
+                      className="object-cover box-front"
+                    />
 
-                  <div
-                    className="box-back w-full h-full flex flex-col items-center justify-center bg-black"
-                    style={{
-                      backgroundImage: "url('/bg_box.jpg')",
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      backgroundRepeat: "no-repeat",
-                    }}
-                  >
-                    <span className="text-white font-bebas text-2xl">
-                      {proj.title}
-                    </span>
-                    <span className="text-white font-bebas text-4xl">
-                      {proj.description}
-                    </span>
+                    <div
+                      className="box-back w-full h-full flex flex-col items-center justify-center bg-black"
+                      style={{
+                        backgroundImage: "url('/bg_box.jpg')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                      }}
+                    >
+                      <span className="text-white font-bebas text-2xl">
+                        {proj.title}
+                      </span>
+                      <span className="text-white font-bebas text-4xl">
+                        {proj.description}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              </TransitionLink>
-            </div>
+                </TransitionLink>
+              </div>
+              {/* FOR MOBILE DEVICES */}
+              <div key={proj.id} className="block sm:hidden w-full h-[240px]">
+                <TransitionLink href={`/projects/${proj.id}`} passHref>
+                  <div className="relative w-full h-full">
+                    <Image
+                      src={proj.img[0]}
+                      alt={proj.alt}
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
+                </TransitionLink>
+              </div>
+            </>
           ))}
         </div>
       </div>
