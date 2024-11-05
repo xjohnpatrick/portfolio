4d3266c5d5e6898db2794f57c5113a1b798a8790
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-05 18:11:26 +0800

    05.11.24.1811

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -29,25 +29,23 @@ export default function ProjectPage({ params }: { params: { id: string } }) {
       <SideNavigation />
       <div className="flex flex-col items-center">
         <h1 className="text-gray font-bebas">{project.website}</h1>
-        <span className="text-white text-3xl font-bebas">
+        <span className="text-white text-3xl font-bebas text-center">
           {project.title} - {project.description}
         </span>
-        <p className="text-gray text-sm text-center">
-          {project.about}
-        </p>
+        <p className="text-gray text-sm text-center">{project.about}</p>
       </div>
       <div className="flex gap-4">
-        <p className="text-white text-[10px]">
+        <p className="flex text-white text-[10px]">
           Role:
-          <span className="text-gray text-[10px]"> {project.role}</span>
+          <span className="text-gray text-[10px] ml-1">{project.role}</span>
         </p>
-        <p className="text-white text-[10px]">
+        <p className="flex text-white text-[10px]">
           Organization:
-          <span className="text-gray text-[10px]"> {project.agency}</span>
+          <span className="flex text-gray text-[10px] ml-1">{project.agency}</span>
         </p>
-        <p className="text-white text-[10px]">
+        <p className="flex text-white text-[10px]">
           Year:
-          <span className="text-gray text-[10px]"> {project.year}</span>
+          <span className="text-gray text-[10px] ml-1">{project.year}</span>
         </p>
       </div>
       {project.link && (
@@ -66,9 +64,14 @@ export default function ProjectPage({ params }: { params: { id: string } }) {
         {project.img.map((image, index) => (
           <div
             key={index}
-            className="relative w-full sm:w-[600px] md:w-[800px] lg:w-[1000px] xl:w-[1200px] h-64 sm:h-80 md:h-96 lg:h-[600px] revealLeft"
+            className="relative w-full sm:w-[600px] md:w-[800px] lg:w-[1000px] xl:w-[1200px] h-40 sm:h-80 md:h-96 lg:h-[600px] revealLeft"
           >
-            <Image src={image} alt={project.alt} fill />
+            <Image
+              src={image}
+              alt={project.alt}
+              fill
+              className="object-contain lg:object-cover"
+            />
           </div>
         ))}
       </div>
--- components/SideNavigation.tsx
+++ components/SideNavigation.tsx
@@ -18,8 +18,6 @@ export default function SideNavigation() {
   };
 
   return (
-    <>
-      {/* FOR LARGE DEVICES */}
       <div className="hidden lg:flex flex-col justify-center fixed right-0 top-1/2 -translate-y-1/2 z-50 gap-[1px]">
         <div className="boxNav w-10 h-10">
           <div className="boxNav-inner relative w-full h-full">
@@ -66,38 +64,5 @@ export default function SideNavigation() {
           </div>
         </div>
       </div>
-      {/* FOR MOBILE */}
-      <div className="flex lg:hidden flex-col justify-center fixed right-0 top-1/2 -translate-y-1/2 z-50 gap-[1px]">
-        <div className="w-10 h-10">
-          <div className="relative w-full h-full">
-            <button
-              onClick={scrollUp}
-              className="flex w-full h-full absolute bg-black text-white items-center justify-center"
-            >
-              <SlArrowUp size={16} />
-            </button>
-          </div>
-        </div>
-        <TransitionLink href="/all-projects">
-          <div className="w-10 h-10">
-            <div className="w-full h-full relative">
-              <button className="flex bg-black w-full h-full absolute text-white items-center justify-center">
-                <TfiClose size={16} />
-              </button>
-            </div>
-          </div>
-        </TransitionLink>
-        <div className="w-10 h-10">
-          <div className="relative w-full h-full">
-            <button
-              onClick={scrollDown}
-              className="flex w-full h-full absolute bg-black text-white items-center justify-center"
-            >
-              <SlArrowDown size={16} />
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
   );
 }
